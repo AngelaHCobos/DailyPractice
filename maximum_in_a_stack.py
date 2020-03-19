d163428dd1517f8809a6084a634ab06a59ecf209
AngeHCobos <cobos.haydee@gmail.com>
2020-03-19 00:32:00 -0300

    added description

--- maximum_in_a_stack.py
+++ maximum_in_a_stack.py
@@ -1,3 +1,9 @@
+"""
+Implement a class for a stack that supports all the regular functions (push, pop) and an additional function of max() 
+which returns the maximum element in the stack (return None if the stack is empty). Each method should run in constant time.
+"""
+
+
 class MaxStack:
     
     def __init__(self):
@@ -19,8 +25,6 @@ class MaxStack:
     def max(self):
         return self.maximo
        
-
-
 s = MaxStack()
 s.push(1)
 s.push(2)
